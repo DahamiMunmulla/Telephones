0d4dfaeb41116eece9de7cd1500d543a04d504e0
Dahami <it20212186@my.sliit.lk>
2021-09-16 18:21:56 +0530

    initial comit

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -42,7 +42,7 @@ export default class Home extends Component{
                 <th scope = "col">SID </th>
                 <th scope = "col">month</th>
                 <th scope = "col">Date</th>
-                <th scope = "col">Action </th>
+                <th scope = "col">action </th>
 
               </tr>
             </thead>
